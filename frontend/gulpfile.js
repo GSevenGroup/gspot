9406e2b65e5e81d67c4ba5371a8f0d9cbaedd769
Lassu Henrik <psykoffein@gmail.com>
2016-01-28 22:09:32 +0100

    This is not the working stuff, just need to commit this shit in...

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -11,6 +11,13 @@ var del = require('del');
 var runSequence = require('run-sequence');
 var autoprefixer = require('gulp-autoprefixer');
 
+//var app = require('gulp-express');
+//
+//var path = require('path');
+//gulp.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+var packageJSON = require('./package.json');
+
 /**
  *  compile sass task
  */
@@ -33,7 +40,7 @@ gulp.task('sass', function () {
 gulp.task('fonts', function() {
 	return gulp.src('app/fonts/**/*')
 		.pipe(gulp.dest('dist/fonts'))
-})
+});
 
 /**
  * optimalizes all the img files and copies them to dev
@@ -52,7 +59,7 @@ gulp.task('images', function(){
  */
 gulp.task('clean:dist', function() {
 	return del.sync('dist');
-})
+});
 
 /**
  * builds all the js,css etc provided to one file
@@ -77,9 +84,80 @@ gulp.task('browserSync', function () {
 	});
 });
 
+/* ---------------------------------------------- */
+/* THIS IS SOME COPYING STUFF FOR THE DEV TO WORK */
+/* ---------------------------------------------- */
+
+function copyCSSDev() {
+	return gulp.src([
+		'node_modules/angular-material/angular-material.css',
+		'node_modules/angular-material-icons/angular-material-icons.css'
+	], {base: '.'})
+		.pipe(gulp.dest(packageJSON.config.devDir));
+}
+gulp.task('copy::css:dev', copyCSSDev);
+
+function copyThirdPartyDev() {
+	return gulp.src([
+		'node_modules/angular/angular.min.js',
+		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+		'node_modules/angular-animate/angular-animate.min.js',
+		'node_modules/angular-aria/angular-aria.min.js',
+		'node_modules/angular-material/angular-material.min.js',
+		'node_modules/angular-material-icons/angular-material-icons.min.js',
+		'node_modules/angular-messages/angular-messages.min.js'
+
+	], {base: '.'})
+		.pipe(gulp.dest(packageJSON.config.devDir));
+}
+gulp.task('copy::thirdparty:dev', copyThirdPartyDev);
+
+
+/**
+ * COPYING -----------------------------------------------------------------
+ */
+
+function copyHtml() {
+	return gulp.src('./app/js/index.html')
+		.pipe(gulp.dest(packageJSON.config.destDir));
+}
+gulp.task('copy::html', copyHtml);
+
+function copyCSS() {
+	return gulp.src([
+		'./app/css/myapp.css',
+		'node_modules/angular-material/angular-material.css',
+		'node_modules/angular-material-icons/angular-material-icons.css'
+	])
+		.pipe(gulp.dest(packageJSON.config.destDir+'/css'));
+}
+gulp.task('copy::css', copyCSS);
+
+function copyThirdParty() {
+	return gulp.src([
+		'node_modules/angular/angular.min.js',
+		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+		'node_modules/angular-animate/angular-animate.min.js',
+		'node_modules/angular-aria/angular-aria.min.js',
+		'node_modules/angular-material/angular-material.min.js',
+		'node_modules/angular-material-icons/angular-material-icons.min.js',
+		'node_modules/angular-messages/angular-messages.min.js'
+
+	])
+		.pipe(gulp.dest(packageJSON.config.destdir+'/lib'));
+}
+gulp.task('copy::thirdparty', copyThirdParty);
+
+
+function copyTemplates() {
+	return gulp.src('./app/js/**/*.tpl')
+		.pipe(gulp.dest(packageJSON.config.destdir+'/templates'));
+}
+gulp.task('copy::templates', copyTemplates);
+
 /**
  * grouping of all watch tasks
-*/
+ */
 gulp.task('watch', ['browserSync', 'sass'], function () {
 	gulp.watch('app/scss/**/*.scss', ['sass']);
 	gulp.watch('app/**/*.html', browserSync.reload);
@@ -90,14 +168,14 @@ gulp.task('watch', ['browserSync', 'sass'], function () {
  * build task
  */
 gulp.task('build', function (callback) {
-	runSequence('clean:dist',
+	runSequence('clean:dist', 'copy::thirdparty', 'copy::html', 'copy::templates', 'copy::css',
 		['sass', 'useref', 'images', 'fonts'],
 		callback
 	)
 });
 
 gulp.task('startdev', function (callback) {
-	runSequence(['sass','browserSync', 'watch'],
+	runSequence('clean:dist', 'copy::thirdparty:dev', 'copy::css:dev', ['sass','browserSync', 'watch'],
 		callback
 	)
 });
