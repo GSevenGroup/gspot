37df386d0f5ac4dd85ee3a0f405c903b52e2d9aa
Lassu Henrik <psykoffein@gmail.com>
2016-02-13 00:58:36 +0100

    Working FE and BE Env finallllllyyyyyyy

--- gulpfile.js
+++ gulpfile.js
@@ -1,185 +1,129 @@
+var packageJSON = require('./package.json'); 
+var elixir = require('laravel-elixir');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
-var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
+var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var rename = require('gulp-rename');
+var watch = require('gulp-watch');
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
 
-//var app = require('gulp-express');
-//
-//var path = require('path');
-//gulp.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+var appdist = "./frontend/app/js";
 
-var packageJSON = require('./package.json');
+elixir(function(mix) {
+    mix.less('app.less');
+});
 
 /**
  *  compile sass task
  */
 gulp.task('sass', function () {
-	return gulp.src('app/scss/**/*.scss')
-		.pipe(sass())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('app/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+    return gulp.src('app/scss/**/*.scss')
+      .pipe(sass())
 });
 
-/**
- * copying all the fonts to dist
- */
-gulp.task('fonts', function() {
-	return gulp.src('app/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'))
-});
+gulp.task('start', [ 'build:js', 'copy:thirdparty', 'copy:html', 'copy:templates', 'copy:css']);
+gulp.task('startdev', [ 'builddev:js', 'copy:thirdpartydev', 'copy:html', 'copy:templates', 'copy:css']);
 
-/**
- * optimalizes all the img files and copies them to dev
- */
-gulp.task('images', function(){
-	return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
-		// Caching images that ran through imagemin
-		.pipe(cache(imagemin({
-			interlaced: true
-		})))
-		.pipe(gulp.dest('dist/img'))
-});
+gulp.task('build', [ 'build:js', 'copy:thirdparty', 'copy:html', 'copy:templates']);
 
-/**
- * cleans dist dir
- */
-gulp.task('clean:dist', function() {
-	return del.sync('dist');
-});
 
-/**
- * builds all the js,css etc provided to one file
- */
-gulp.task('useref', function(){
-	return gulp.src('app/*.html')
-		.pipe(useref())
-		// Minifies only if it's a JavaScript file
-		.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulpIf('*.css', cssnano()))
-		.pipe(gulp.dest('dist'))
-});
 
-/**
- * creates the live reload function
- */
-gulp.task('browserSync', function () {
-	browserSync({
-		/*server: {
-		 baseDir: 'app'
-		 }*/
-		proxy: '127.0.0.1:8080',
-		port: 9000,
-		open: true,
-		notify: false
-	});
-});
-
-/* ---------------------------------------------- */
-/* THIS IS SOME COPYING STUFF FOR THE DEV TO WORK */
-/* ---------------------------------------------- */
-
-function copyCSSDev() {
-	return gulp.src([
-		'node_modules/angular-material/angular-material.css',
-		'node_modules/angular-material-icons/angular-material-icons.css'
-	], {base: '.'})
-		.pipe(gulp.dest(packageJSON.config.devDir));
+function resetDist() {
+	return gulp.src('./public/dist', {read: false})
+		.pipe(clean());
 }
-gulp.task('copy::css:dev', copyCSSDev);
+gulp.task('resetdist', resetDist);
 
-function copyThirdPartyDev() {
-	return gulp.src([
-		'node_modules/angular/angular.min.js',
-		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
-		'node_modules/angular-animate/angular-animate.min.js',
-		'node_modules/angular-aria/angular-aria.min.js',
-		'node_modules/angular-material/angular-material.min.js',
-		'node_modules/angular-material-icons/angular-material-icons.min.js',
-		'node_modules/angular-messages/angular-messages.min.js'
-
-	], {base: '.'})
-		.pipe(gulp.dest(packageJSON.config.devDir));
+function buildScripts() {
+    gulp.src([
+        appdist+'/**/*controller.js',
+        appdist+'/**/*filter.js',
+        appdist+'/**/*service.js',
+        appdist+'/**/*factory.js',
+        appdist+'/**/*config.js',
+        appdist+'/**/*directive.js',
+        appdist+'/**/*run.js',
+        appdist+'/**/*module.js',
+        appdist+'/index.js'
+    ])
+    .pipe(concat('myapp.js'))        
+    .pipe(ngAnnotate({add: true}))
+    .pipe(uglify({mangle: true }))
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(packageJSON.config.destdir));
 }
-gulp.task('copy::thirdparty:dev', copyThirdPartyDev);
+gulp.task('build:js', buildScripts);
 
-
-/**
- * COPYING -----------------------------------------------------------------
- */
-
-function copyHtml() {
-	return gulp.src('./app/js/index.html')
-		.pipe(gulp.dest(packageJSON.config.destDir));
+function buildScripts() {
+    gulp.src([
+         appdist+'/**/*controller.js',
+         appdist+'/**/*filter.js',
+         appdist+'/**/*service.js',
+         appdist+'/**/*factory.js',
+         appdist+'/**/*config.js',
+         appdist+'/**/*directive.js',
+         appdist+'/**/*run.js',
+         appdist+'/**/*module.js',
+         appdist+'/index.js'
+    ])
+    .pipe(concat('myapp.js'))        
+    .pipe(ngAnnotate({add: true}))
+    .pipe(gulp.dest(packageJSON.config.destdir));
 }
-gulp.task('copy::html', copyHtml);
-
-function copyCSS() {
-	return gulp.src([
-		'./app/css/myapp.css',
-		'node_modules/angular-material/angular-material.css',
-		'node_modules/angular-material-icons/angular-material-icons.css'
-	])
-		.pipe(gulp.dest(packageJSON.config.destDir+'/css'));
-}
-gulp.task('copy::css', copyCSS);
+gulp.task('builddev:js', buildScripts);
 
 function copyThirdParty() {
-	return gulp.src([
-		'node_modules/angular/angular.min.js',
-		'node_modules/angular-ui-router/release/angular-ui-router.min.js',
-		'node_modules/angular-animate/angular-animate.min.js',
-		'node_modules/angular-aria/angular-aria.min.js',
-		'node_modules/angular-material/angular-material.min.js',
-		'node_modules/angular-material-icons/angular-material-icons.min.js',
-		'node_modules/angular-messages/angular-messages.min.js'
-
-	])
-		.pipe(gulp.dest(packageJSON.config.destdir+'/lib'));
+    return gulp.src([
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+        'node_modules/angular-animate/angular-animate.min.js',
+        'node_modules/angular-aria/angular-aria.min.js',
+        'node_modules/angular-material/angular-material.min.js',
+        'node_modules/angular-material-icons/angular-material-icons.min.js',
+        'node_modules/angular-messages/angular-messages.min.js'
+        
+    ])
+    .pipe(gulp.dest(packageJSON.config.destdir+'/lib'));
 }
-gulp.task('copy::thirdparty', copyThirdParty);
+gulp.task('copy:thirdparty', copyThirdParty);
+
+function copyThirdPartyDev() {
+    return gulp.src([
+        'node_modules/angular/angular.js',
+        'node_modules/angular-ui-router/release/angular-ui-router.js',
+        'node_modules/angular-animate/angular-animate.js',
+        'node_modules/angular-aria/angular-aria.js',
+        'node_modules/angular-material/angular-material.js',
+        'node_modules/angular-material-icons/angular-material-icons.js',
+        'node_modules/angular-messages/angular-messages.js'
+    ])
+    .pipe(gulp.dest(packageJSON.config.destdir+'/lib'));
+}
+gulp.task('copy:thirdpartydev', copyThirdPartyDev);
+
+
+function copyHtml() {
+    return gulp.src(appdist+'/index.php')
+    .pipe(gulp.dest("./resources/views"));
+}
+gulp.task('copy:html', copyHtml);
 
 
 function copyTemplates() {
-	return gulp.src('./app/js/**/*.tpl')
-		.pipe(gulp.dest(packageJSON.config.destdir+'/templates'));
+    return gulp.src(appdist+'/**/*.tpl')
+    .pipe(gulp.dest(packageJSON.config.destdir+'/templates'));
 }
-gulp.task('copy::templates', copyTemplates);
+gulp.task('copy:templates', copyTemplates);
 
-/**
- * grouping of all watch tasks
- */
-gulp.task('watch', ['browserSync', 'sass'], function () {
-	gulp.watch('app/scss/**/*.scss', ['sass']);
-	gulp.watch('app/**/*.html', browserSync.reload);
-	gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-/**
- * build task
- */
-gulp.task('build', function (callback) {
-	runSequence('clean:dist', 'copy::thirdparty', 'copy::html', 'copy::templates', 'copy::css',
-		['sass', 'useref', 'images', 'fonts'],
-		callback
-	)
-});
-
-gulp.task('startdev', function (callback) {
-	runSequence('clean:dist', 'copy::thirdparty:dev', 'copy::css:dev', ['sass','browserSync', 'watch'],
-		callback
-	)
-});
+function copyCSS() {
+    return gulp.src([
+         './frontend/app//css/myapp.css',
+        'node_modules/angular-material/angular-material.css',
+        'node_modules/angular-material-icons/angular-material-icons.css'
+    ])
+    .pipe(gulp.dest(packageJSON.config.destdir+'/css'));
+}
+gulp.task('copy:css', copyCSS);
